08f6e3ac9b4ff252bf1cff73548951b6d94fa2fd
Habeeb <h.makusota@outlook.com>
2021-01-20 18:37:32 +0100

    great quotes

--- scripts.js
+++ scripts.js
@@ -3,9 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 //Get Quote from API
 async function getQuote() {
+  loading();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
    try {
@@ -24,12 +41,16 @@ async function getQuote() {
         }else {
           quoteText.classList.remove('long-quote');
         }
-        quoteText.innerText = data.quoteText
+        
+        quoteText.innerText = data.quoteText;
+        
+        // Stop Loader, Show Quote
+        complete(); 
    } catch (error) {
      getQuote();
-    
    }
 }
+
 // Tweet Quote
 function tweetQuote() {
   const quote = quoteText.innerText;
@@ -40,7 +61,7 @@ function tweetQuote() {
 
 // Event Listeners
 newQuoteBtn.addEventListener('click', getQuote);
-twitterBtn,addEventListener('click', tweetQuote);
-
+twitterBtn.addEventListener('click', tweetQuote);
+ 
 //on load
 getQuote();
